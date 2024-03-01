68d0ccefdd41d7e2f395c80a5b24a290493a3eac
tv220196 <tv220196@gmail.com>
2024-03-01 10:54:01 +0500

    ver1

--- spreadsheet/FindANTLR.cmake
+++ spreadsheet/FindANTLR.cmake
@@ -1,10 +1,10 @@
 find_package(Java QUIET COMPONENTS Runtime)
-
+ 
 if(NOT ANTLR_EXECUTABLE)
   find_program(ANTLR_EXECUTABLE
-               NAMES antlr.jar antlr4.jar antlr-4.jar antlr-4.7.2-complete.jar)
+               NAMES antlr.jar antlr4.jar antlr-4.jar antlr-4.12.0-complete.jar)
 endif()
-
+ 
 if(ANTLR_EXECUTABLE AND Java_JAVA_EXECUTABLE)
   execute_process(
       COMMAND ${Java_JAVA_EXECUTABLE} -jar ${ANTLR_EXECUTABLE}
@@ -12,9 +12,9 @@ if(ANTLR_EXECUTABLE AND Java_JAVA_EXECUTABLE)
       ERROR_VARIABLE ANTLR_COMMAND_ERROR
       RESULT_VARIABLE ANTLR_COMMAND_RESULT
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-
+ 
   if(ANTLR_COMMAND_RESULT EQUAL 0)
-    string(REGEX MATCH "Version [0-9]+(\\.[0-9])*" ANTLR_VERSION ${ANTLR_COMMAND_OUTPUT})
+    string(REGEX MATCH "Version [0-9]+(\\.[0-9]+)*" ANTLR_VERSION ${ANTLR_COMMAND_OUTPUT})
     string(REPLACE "Version " "" ANTLR_VERSION ${ANTLR_VERSION})
   else()
     message(
@@ -22,7 +22,7 @@ if(ANTLR_EXECUTABLE AND Java_JAVA_EXECUTABLE)
         "Command '${Java_JAVA_EXECUTABLE} -jar ${ANTLR_EXECUTABLE}' "
         "failed with the output '${ANTLR_COMMAND_ERROR}'")
   endif()
-
+ 
   macro(ANTLR_TARGET Name InputFile)
     set(ANTLR_OPTIONS LEXER PARSER LISTENER VISITOR)
     set(ANTLR_ONE_VALUE_ARGS PACKAGE OUTPUT_DIRECTORY DEPENDS_ANTLR)
@@ -32,20 +32,20 @@ if(ANTLR_EXECUTABLE AND Java_JAVA_EXECUTABLE)
                           "${ANTLR_ONE_VALUE_ARGS}"
                           "${ANTLR_MULTI_VALUE_ARGS}"
                           ${ARGN})
-
+ 
     set(ANTLR_${Name}_INPUT ${InputFile})
-
+ 
     get_filename_component(ANTLR_INPUT ${InputFile} NAME_WE)
-
+ 
     if(ANTLR_TARGET_OUTPUT_DIRECTORY)
       set(ANTLR_${Name}_OUTPUT_DIR ${ANTLR_TARGET_OUTPUT_DIRECTORY})
     else()
       set(ANTLR_${Name}_OUTPUT_DIR
           ${CMAKE_CURRENT_BINARY_DIR}/antlr4cpp_generated_src/${ANTLR_INPUT})
     endif()
-
+ 
     unset(ANTLR_${Name}_CXX_OUTPUTS)
-
+ 
     if((ANTLR_TARGET_LEXER AND NOT ANTLR_TARGET_PARSER) OR
        (ANTLR_TARGET_PARSER AND NOT ANTLR_TARGET_LEXER))
       list(APPEND ANTLR_${Name}_CXX_OUTPUTS
@@ -64,7 +64,7 @@ if(ANTLR_EXECUTABLE AND Java_JAVA_EXECUTABLE)
            ${ANTLR_${Name}_OUTPUT_DIR}/${ANTLR_INPUT}Lexer.interp
            ${ANTLR_${Name}_OUTPUT_DIR}/${ANTLR_INPUT}Lexer.tokens)
     endif()
-
+ 
     if(ANTLR_TARGET_LISTENER)
       list(APPEND ANTLR_${Name}_CXX_OUTPUTS
            ${ANTLR_${Name}_OUTPUT_DIR}/${ANTLR_INPUT}BaseListener.h
@@ -73,7 +73,7 @@ if(ANTLR_EXECUTABLE AND Java_JAVA_EXECUTABLE)
            ${ANTLR_${Name}_OUTPUT_DIR}/${ANTLR_INPUT}Listener.cpp)
       list(APPEND ANTLR_TARGET_COMPILE_FLAGS -listener)
     endif()
-
+ 
     if(ANTLR_TARGET_VISITOR)
       list(APPEND ANTLR_${Name}_CXX_OUTPUTS
            ${ANTLR_${Name}_OUTPUT_DIR}/${ANTLR_INPUT}BaseVisitor.h
@@ -82,13 +82,13 @@ if(ANTLR_EXECUTABLE AND Java_JAVA_EXECUTABLE)
            ${ANTLR_${Name}_OUTPUT_DIR}/${ANTLR_INPUT}Visitor.cpp)
       list(APPEND ANTLR_TARGET_COMPILE_FLAGS -visitor)
     endif()
-
+ 
     if(ANTLR_TARGET_PACKAGE)
       list(APPEND ANTLR_TARGET_COMPILE_FLAGS -package ${ANTLR_TARGET_PACKAGE})
     endif()
-
+ 
     list(APPEND ANTLR_${Name}_OUTPUTS ${ANTLR_${Name}_CXX_OUTPUTS})
-
+ 
     if(ANTLR_TARGET_DEPENDS_ANTLR)
       if(ANTLR_${ANTLR_TARGET_DEPENDS_ANTLR}_INPUT)
         list(APPEND ANTLR_TARGET_DEPENDS
@@ -100,7 +100,7 @@ if(ANTLR_EXECUTABLE AND Java_JAVA_EXECUTABLE)
                 "ANTLR target '${ANTLR_TARGET_DEPENDS_ANTLR}' not found")
       endif()
     endif()
-
+ 
     add_custom_command(
         OUTPUT ${ANTLR_${Name}_OUTPUTS}
         COMMAND ${Java_JAVA_EXECUTABLE} -jar ${ANTLR_EXECUTABLE}
@@ -114,9 +114,9 @@ if(ANTLR_EXECUTABLE AND Java_JAVA_EXECUTABLE)
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         COMMENT "Building ${Name} with ANTLR ${ANTLR_VERSION}")
   endmacro(ANTLR_TARGET)
-
+ 
 endif(ANTLR_EXECUTABLE AND Java_JAVA_EXECUTABLE)
-
+ 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     ANTLR
--- spreadsheet/cell.h
+++ spreadsheet/cell.h
@@ -1,36 +1,76 @@
 #pragma once
-
+ 
 #include "common.h"
 #include "formula.h"
-
+ 
 #include <functional>
+#include <optional>
+#include <set>
+#include <stack>
 #include <unordered_set>
 
 class Sheet;
-
+ 
 class Cell : public CellInterface {
 public:
     Cell(Sheet& sheet);
     ~Cell();
-
+ 
     void Set(std::string text);
     void Clear();
-
+ 
     Value GetValue() const override;
     std::string GetText() const override;
     std::vector<Position> GetReferencedCells() const override;
-
+    
     bool IsReferenced() const;
-
+    void InvalidateAllCache(bool flag);
+    
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
-
+    class Impl {
+    public:
+        virtual ~Impl() = default;
+        virtual Value GetValue() const = 0;
+        virtual std::string GetText() const = 0;
+        virtual std::vector<Position> GetReferencedCells() const;
+        virtual bool HasCache();
+        virtual void InvalidateCache();
+    };
+    
+    class EmptyImpl : public Impl {
+    public:    
+        Value GetValue() const override;
+        std::string GetText() const override;      
+    };
+    
+    class TextImpl : public Impl {
+    public:
+        explicit TextImpl(std::string text); 
+        Value GetValue() const override;       
+        std::string GetText() const override;
+        
+    private:
+        std::string text_;        
+    };
+    
+    class FormulaImpl : public Impl {
+    public:
+        explicit FormulaImpl(std::string text, SheetInterface& sheet);
+        Value GetValue() const override;
+        std::string GetText() const override;
+        std::vector<Position> GetReferencedCells() const override;
+        bool HasCache() override;
+        void InvalidateCache() override;
+        
+    private:
+        mutable std::optional<FormulaInterface::Value> cache_;
+        std::unique_ptr<FormulaInterface> formula_ptr_;
+        SheetInterface& sheet_;
+    };
+    
     std::unique_ptr<Impl> impl_;
-
-    // Добавьте поля и методы для связи с таблицей, проверки циклических 
-    // зависимостей, графа зависимостей и т. д.
-
+    Sheet& sheet_;
+    
+    std::set<Cell*> dependent_cells_;
+    std::set<Cell*> referenced_cells_;
 };
